dae2068d493257543634f1aeb616d3abc4d9e032
unstephenk <unstephenk@gmail.com>
2024-02-27 15:19:50 -0600

    setup-347-top-k-frequent-elements

---
+++ 347-Top-K-Frequent-Elements/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+
+	value := []int{1, 1, 1, 2, 2, 3}
+	k := 2
+
+	// value := [1]int{1}
+	// k := 1
+	topKFrequent(value, k)
+}
+
+func topKFrequent(nums []int, k int) []int {
+
+}
