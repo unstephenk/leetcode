3a4f3d5b1f352341c7976c787531ed113b17dedf
unstephenk <unstephenk@gmail.com>
2024-03-21 14:21:40 -0500

    setup

---
+++ 125-Valid-Palindrome/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	value := "A man, a plan, a canal: Panama"
+
+	res := isPalindrome(value)
+
+	fmt.Println(res)
+}
+
+func isPalindrome(s string) bool {
+
+}
