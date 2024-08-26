60cbe230cbc68ef9967992fc3bfb1198e1e3d487
Stephen Kuehl <unstephenk@gmail.com>
2024-08-26 09:36:27 -0500

    update

---
+++ second-attempt/238-product-of-array-sxcept-self/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+	// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+	// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+	nums := []int{1, 2, 3, 4}
+
+	res := productExceptSelf(nums)
+
+	fmt.Println(res)
+
+}
+
+func productExceptSelf(nums []int) []int {
+	answer := make([]int, len(nums))
+
+	prefixPointer := 1
+
+	// add the prefixes to the answer array
+	for currIndex, currValue := range nums {
+		answer[currIndex] = prefixPointer
+		prefixPointer *= currValue
+	}
+
+	postfixPointer := 1
+
+	// mulitply the answer value with the postfixPointer
+	for i := len(nums) - 1; i >= 0; i-- {
+		answer[i] *= postfixPointer
+		postfixPointer *= nums[i]
+	}
+
+	return answer
+}
