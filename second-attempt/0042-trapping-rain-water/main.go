2388788e9080a48e2e7872b9965d4077ac1b4f46
unstephenk <unstephenk@gmail.com>
2024-09-16 14:21:05 -0500

    updates

--- second-attempt/0042-trapping-rain-water/main.go
+++ second-attempt/0042-trapping-rain-water/main.go
@@ -7,8 +7,9 @@ func main() {
 	// compute how much water it can trap after raining.
 
 	height := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
-	res := trap(height)
-	fmt.Println(res)
+	res1 := trap(height)
+	res2 := rainWaterPreCalc(height)
+	fmt.Println(res1, res2)
 }
 
 func trap(height []int) int {
@@ -51,4 +52,37 @@ func min(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func rainWaterPreCalc(height []int) int {
+	// precalculate the highest columns for left and right
+	// add them to arrays
+	// step through the array again
+	// calculate the volume
+
+	res := 0
+	lengthArr := len(height)
+
+	// precalulate the left side
+	left := make([]int, len(height))
+	left[0] = height[0]
+	// do not include the first element
+	for i := 1; i < len(height); i++ {
+		left[i] = max(left[i-1], height[i])
+	}
+
+	// do not include the last element
+	right := make([]int, len(height))
+	right[lengthArr-1] = height[lengthArr-1]
+	for i := len(height) - 2; i >= 0; i-- {
+		right[i] = max(right[i+1], height[i])
+	}
+
+	// both arrrays are populated, run calculation
+	for i := 0; i < lengthArr; i++ {
+		res += min(left[i], right[i]) - height[i]
+	}
+
+	return res
+
 }
