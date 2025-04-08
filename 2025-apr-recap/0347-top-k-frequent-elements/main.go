281a1786c8bf2d64d50cd8ea873ea3c2ee932554
unstephenk <unstephenk@gmail.com>
2025-04-08 14:08:58 -0500

    updates

--- 2025-apr-recap/0347-top-k-frequent-elements/main.go
+++ 2025-apr-recap/0347-top-k-frequent-elements/main.go
@@ -6,6 +6,11 @@ func main() {
 
 	// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 
+	// CATEGORY: Arrays & Hashing
+	// TIME: O(n) - n is the size of the input array
+	// SPACE: O(n) - n is the size of the input array
+	// ALGO: Bucket Sort
+
 	nums := []int{1, 1, 1, 2, 2, 3}
 	k := 2
 
@@ -21,31 +26,27 @@ func topKFrequent(nums []int, k int) []int {
 	// convert the first map to a Bucket Sort
 	// start at the last element in the bucket sort and find the k most frequent values
 
-	frequencyMap := make(map[int]int, len(nums))
+	cntMap := make(map[int]int)
+	freq := make([][]int, len(nums)+1)
 
-	// { nums: frequency }
 	for _, numsValue := range nums {
-		frequencyMap[numsValue]++
+		cntMap[numsValue]++
 	}
 
-	// [[0], [numsValue], [numsValue]]
-	bucketArray := make([][]int, len(nums)+1)
-
-	for numsValue, frequencyCount := range frequencyMap {
-		bucketArray[frequencyCount] = append(bucketArray[frequencyCount], numsValue)
+	for num, cnt := range cntMap {
+		freq[cnt] = append(freq[cnt], num)
 	}
 
-	answerArray := make([]int, 0, len(nums))
+	res := []int{}
 
-	for i := len(bucketArray) - 1; i >= 0; i-- {
-		for _, bucketValue := range bucketArray[i] {
-			if k > 0 {
-				answerArray = append(answerArray, bucketValue)
-				k--
+	for i := len(freq) - 1; i > 0; i-- {
+		for _, val := range freq[i] {
+			res = append(res, val)
+			if len(res) == k {
+				return res
 			}
 		}
 	}
 
-	return answerArray
-
+	return res
 }
