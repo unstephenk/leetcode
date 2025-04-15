4c499ec50c9e57f8867dea4d7e4324b4d1b900d2
unstephenk <unstephenk@gmail.com>
2025-04-15 15:38:09 -0500

    updates

--- 2025-apr-recap/0128-longest-consecutive-sequence/main.go
+++ 2025-apr-recap/0128-longest-consecutive-sequence/main.go
@@ -7,6 +7,8 @@ func main() {
 	// You must write an algorithm that runs in O(n) time.
 
 	// CATEGORY: Arrays & Hashing
+	// TIME: O(n)
+	// SPACE: O(n)
 
 	nums := []int{100, 4, 200, 1, 3, 2}
 
@@ -17,4 +19,31 @@ func main() {
 }
 
 func longestConsecutive(nums []int) int {
+	numsMap := make(map[int]bool, len(nums))
+	res := 0
+
+	for _, numsValue := range nums {
+		numsMap[numsValue] = true
+	}
+
+	for item := range numsMap {
+		if _, ok := numsMap[item-1]; !ok { // this is the start of the sequence
+
+			cnt := 1
+			for _, ok := numsMap[item+cnt]; ok; _, ok = numsMap[item+cnt] {
+				cnt++
+			}
+
+			res = max(cnt, res)
+		}
+	}
+
+	return res
+}
+
+func max(newLength, longest int) int {
+	if newLength > longest {
+		return newLength
+	}
+	return longest
 }
--- all-answers/0128-longest-consecutive-sequence/main.go
+++ all-answers/0128-longest-consecutive-sequence/main.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+	// You must write an algorithm that runs in O(n) time.
+
+	// CATEGORY: Arrays & Hashing
+	// TIME: O(n)
+	// SPACE: O(n)
 
 	nums := []int{100, 4, 200, 1, 3, 2}
 
