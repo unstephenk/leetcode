75ef27224c8901fe265a005c3d60098861900463
unstephenk <unstephenk@gmail.com>
2025-06-10 14:45:38 -0500

    updates

--- 2025-june/0049-group-anagrams/main.go
+++ 2025-june/0049-group-anagrams/main.go
@@ -17,5 +17,31 @@ func main() {
 }
 
 func groupAnagrams(strs []string) [][]string {
+	// travese the array
+	// traverse the string
+	// for each rune in the string, convert to its alpha value in a 26 index array
+	// add that array and the string value to the hashmap
+	// convert the hashmap to a nested array and return the answer
 
+	anagramMap := map[[26]int][]string{}
+
+	for _, strArray := range strs {
+
+		tempArray := [26]int{}
+
+		for _, charVal := range strArray {
+
+			tempArray[charVal-'a']++
+		}
+
+		anagramMap[tempArray] = append(anagramMap[tempArray], strArray)
+	}
+
+	ansArray := [][]string{}
+
+	for _, mapStrings := range anagramMap {
+		ansArray = append(ansArray, mapStrings)
+	}
+
+	return ansArray
 }
