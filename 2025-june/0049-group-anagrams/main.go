2859446e654e4f28cc3284f87e2a99b1f9e88f33
unstephenk <unstephenk@gmail.com>
2025-06-10 14:45:26 -0500

    updates

---
+++ 2025-june/0049-group-anagrams/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Given an array of strings strs, group the anagrams together.
+	// You can return the answer in any order.
+
+	// CATEGORY: Arrays & Hashing
+	// TIME COMPLEXITY:
+
+	strs := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
+
+	res := groupAnagrams(strs)
+	fmt.Println(res)
+
+}
+
+func groupAnagrams(strs []string) [][]string {
+
+}
